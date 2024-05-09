b216acc73b4cad7322fe295615f98b7c4d4d6ada
Pedro M. <pedrom.sanches@outlook.com>
2024-05-09 16:30:40 -0300

    fix and enhance unhandled exception handler; add -Weverything

--- src/main.cc
+++ src/main.cc
@@ -10,18 +10,29 @@
 auto global_exception_handler() -> void {
     // Emergency version of app::util::scream_and_abort
     const auto stacktrace = std::stacktrace::current(1);
+    const auto current_exception = std::current_exception();
 
 #ifdef __GNUG__
-    const auto current_exception_type = abi::__cxa_current_exception_type()->name();
+    const auto current_exception_type_name = abi::__cxa_current_exception_type()->name();
     std::println("uncaught exception of type {}",
-        app::util::cxx_demangle(current_exception_type)
+        app::util::cxx_demangle(current_exception_type_name)
     );
 #else
-    std::println("uncaught exception.");
+    std::println("uncaught exception (type is not known)");
 #endif
+    
+    // worst c++ mechanisms
+    try {
+        if (current_exception)
+            std::rethrow_exception(current_exception);
+    } catch (const std::exception& e) {
+        std::println("  what: {}", e.what());
+    } catch (...) {
+        std::println("  exception message is not known.");
+    }
 
     app::util::print_stacktrace(std::move(stacktrace));
-    std::terminate();
+    std::abort();
 }
 
 auto main(int argc, char** argv) -> int {
